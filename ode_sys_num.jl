a4cf99998b802bb67207fba072fea0b72c4b10f0
Andre Guerra <andre.guerra@mail.mcgill.ca>
2023-04-04 05:38:53 -0400

    added the numerical solution script and figures

---
+++ ode_sys_num.jl
@@ -0,0 +1,42 @@
+using ModelingToolkit
+using DifferentialEquations
+import Plots.plot, Plots.savefig
+
+# define parameters, variables, and d/dt operator
+@parameters t
+@variables x(..) y(..) z(..)
+Dt = Differential(t)
+
+# system of ODEs
+eqs = [Dt(x(t)) ~ -x(t) + 3*z(t),
+       Dt(y(t)) ~ -y(t) + 2*z(t),
+       Dt(z(t)) ~ x(t)^2 - 2*z(t)]
+# initial conditions
+ics = [0,1/2,3]
+
+# define the problem using DifferentialEquations.jl
+@named odesystem = ODESystem(eqs,t,tspan=(0,1.5))
+prob = ODEProblem(odesystem,ics)
+# solve the system
+sol = solve(prob,Tsit5())
+
+# visualize sol
+plot(sol, label=["x" "y" "z"], title="Numerical Solution",
+     linewidth=2, xlabel="t");
+savefig("ode_sys_num_sol")
+
+# sol is a vector of vectors, convert to matrix
+solution = reduce(vcat,sol.u')
+
+# use the matrix to plot solution
+plot(sol.t, [solution[:,1] solution[:,2] solution[:,3]],
+     linewidth=2, label=["x" "y" "z"], xlabel="t");
+savefig("ode_sys_num_matrix")
+# subplots of x, y, and z solutions
+plot(sol.t, [solution[:,1] solution[:,2] solution[:,3]],
+     layout=(3,1), linewidth=2, label=["x" "y" "z"]);
+savefig("ode_sys_num_subplots")
+# 3D plot
+plot(solution[:,1], solution[:,2], solution[:,3], label="(x,y,z)",
+     linewidth=2, title="3D space solution");
+savefig("ode_sys_num_3D")
